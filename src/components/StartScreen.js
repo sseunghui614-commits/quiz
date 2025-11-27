87a507cf73368480d417e92d517181458cc07c18
sseunghui614 <sseunghui614@gmail.com>
2025-11-27 17:57:01 +0900

    quizappmiddle

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,7 +1,9 @@
 const StartScreen = ({ onStart }) => {
     return (
         <div id="start-screen">
-        <div className="logo-mark">📚</div>
+        <div className="logo-mark">
+            <img src="/images/quizfinish.png" alt="quiz logo" />
+        </div>
         <p className="start-subtitle">한국사를 한눈에</p>
         <button className="primary-btn" onClick={onStart}>
             시작
